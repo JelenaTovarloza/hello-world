742732c80b09627f683eaa0770ca0e7052de9799
JelenaTovarloza <j.tovarloza@gmail.com>
2025-03-30 18:22:46 +0200

    Add files via upload

--- test.bat
+++ test.bat
@@ -1,1 +1,13 @@
+echo ================================
+echo Jenkins Test - Primer 2
+echo ================================
 
+echo Provera da li postoji fajl important.txt...
+
+IF EXIST important.txt (
+    echo [PASSED] Fajl important.txt postoji.
+) ELSE (
+    echo [FAILED] Fajl important.txt NE postoji.
+)
+
+echo Test zavrsen.
